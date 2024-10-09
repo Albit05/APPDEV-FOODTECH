05ccc7dc2f9fc1c9321dc813c1c8df5c8362c6c8
Albit05 <anfred20@gmail.com>
2024-10-09 21:54:15 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APPDEV_FOODTECH',
+  webDir: 'www'
+};
+
+export default config;
